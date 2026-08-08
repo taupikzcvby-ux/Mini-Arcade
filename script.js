ce67961e346ef9ab14767d2d0bc3a53fa0defe6f
Hamz Von Vermilion <taupikzcvby@gmail.com>
2026-08-08 12:55:06 +0700

    Add function to open specified game

---
+++ script.js
@@ -0,0 +1,19 @@
+function openGame(game) {
+
+    if (game === "dino") {
+        window.location.href = "games/dino/index.html";
+    }
+
+    else if (game === "snake") {
+        window.location.href = "games/snake/index.html";
+    }
+
+    else if (game === "shooter") {
+        window.location.href = "games/shooter/index.html";
+    }
+
+    else if (game === "memory") {
+        window.location.href = "games/memory/index.html";
+    }
+
+}
